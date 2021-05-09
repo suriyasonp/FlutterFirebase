121c2b9ba3012e7b31c0260ec95b7fd3d5df043d
Suriya <suriya.sonphu@gmail.com>
2021-05-09 22:50:04 +0700

    add cloud_firestore

--- lib/screens/formscreen.dart
+++ lib/screens/formscreen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/model/student.dart';
 import 'package:form_field_validator/form_field_validator.dart';
@@ -12,90 +13,114 @@ class _FormScreenState extends State<FormScreen> {
 
   Students myStudent = Students();
 
+  final Future<FirebaseApp> firebase = Firebase.initializeApp();
+
   final requiredValidator = RequiredValidator(errorText: 'จำเป็นต้องระบุ');
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("แบบฟอร์มบันทึกคะแนนสอบ"),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Form(
-          key: formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "ชื่อ",
-                  style: TextStyle(fontSize: 20),
-                ),
-                TextFormField(
-                  validator: requiredValidator,
-                  onSaved: (String firstName) {
-                    myStudent.firstName = firstName;
-                  },
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  "สกุล",
-                  style: TextStyle(fontSize: 16),
-                ),
-                TextFormField(
-                  onSaved: (String lastName) {
-                    myStudent.lastName = lastName;
-                  },
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  "อีเมล์",
-                  style: TextStyle(fontSize: 16),
-                ),
-                TextFormField(
-                  validator: EmailValidator(errorText: "กรุณาระบุอีเมล์"),
-                  onSaved: (String email) {
-                    myStudent.email = email;
-                  },
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  "คะแนน",
-                  style: TextStyle(fontSize: 16),
-                ),
-                TextFormField(
-                  validator: requiredValidator,
-                  onSaved: (String score) {
-                    myStudent.score = score;
-                  },
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    child: Text("บันทึก"),
-                    onPressed: () {
-                      if (formKey.currentState.validate()) {
-                        formKey.currentState.save();
-                        print("${myStudent.firstName}");
-                        print("${myStudent.lastName}");
-                        print("${myStudent.email}");
-                        print("${myStudent.score}");
-                      }
-                    },
-                  ),
-                )
-              ],
+    return FutureBuilder(
+      future: firebase,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Error"),
             ),
+            body: Center(
+              child: Text(snapshot.error),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("แบบฟอร์มบันทึกคะแนนสอบ"),
+            ),
+            body: Container(
+              padding: EdgeInsets.all(20),
+              child: Form(
+                key: formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "ชื่อ",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      TextFormField(
+                        validator: requiredValidator,
+                        onSaved: (String firstName) {
+                          myStudent.firstName = firstName;
+                        },
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "สกุล",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      TextFormField(
+                        onSaved: (String lastName) {
+                          myStudent.lastName = lastName;
+                        },
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "อีเมล์",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      TextFormField(
+                        validator: EmailValidator(errorText: "กรุณาระบุอีเมล์"),
+                        onSaved: (String email) {
+                          myStudent.email = email;
+                        },
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "คะแนน",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      TextFormField(
+                        validator: requiredValidator,
+                        onSaved: (String score) {
+                          myStudent.score = score;
+                        },
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          child: Text("บันทึก"),
+                          onPressed: () {
+                            if (formKey.currentState.validate()) {
+                              formKey.currentState.save();
+                              print("${myStudent.firstName}");
+                              print("${myStudent.lastName}");
+                              print("${myStudent.email}");
+                              print("${myStudent.score}");
+                            }
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        }
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
